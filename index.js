d9c775112c97212dab8add2e74f8a20945b80636
Nikolay <nik.tarnavsky@gmail.com>
2023-12-26 17:07:18 +0200

    Homework done

---
+++ index.js
@@ -0,0 +1,7 @@
+const numOne = Number(prompt('Введіть перше число'))
+const numTwo = Number(prompt('Введіть друге число'))
+const numThree = Number(prompt('Введіть третє число'))
+
+const result = (numOne + numTwo + numThree) / 3;
+
+alert(result)
