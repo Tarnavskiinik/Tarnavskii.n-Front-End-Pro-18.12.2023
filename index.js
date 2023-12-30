835284385bc6abf98e4f37661904fa88742e979a
Nikolay <nik.tarnavsky@gmail.com>
2023-12-30 14:31:35 +0200

    Homework done

--- index.js
+++ index.js
@@ -2,30 +2,36 @@ const age = Number(prompt('Enter your year of birth'));
 const city = prompt('Enter your city');
 const sport = prompt('Enter your favorite sport');
 
-if(age){
-    alert(2023 - age)
-} else if( age === null){
-    alert('Too bad you did not want to enter your age')
+let message = '';
+
+if (age) {
+    message += `Тебе: ${2023 - age}\n`;
+} else if (age === null) {
+    message += 'Too bad you did not want to enter your age\n';
 }
 
-if(city === 'Киев'){
-    alert('Ты живешь в Столице Украины')
-}else if (city === 'Вашингтон'){
-    alert('Ты живешь в Столице Америки')
-}else if(city === 'Лондон'){
-    alert('Ты живешь в Столице Англии')
-}else if(city === null){
-    alert('Too bad you did not want to enter your city')
-}else{
-    alert('Ты живешь в городе ' + citi)
+if (city === 'Киев') {
+    message += 'Ты живешь в столице Украины\n';
+} else if (city === 'Вашингтон') {
+    message += 'Ты живешь в столице Америки\n';
+} else if (city === 'Лондон') {
+    message += 'Ты живешь в столице Англии\n';
+} else if (city === null) {
+    message += 'Too bad you did not want to enter your city\n';
+} else {
+    message += `Ты живешь в городе  ${city} \n`;
 }
 
-if (sport === 'boks'){
-    alert('Круто! Ты хочешь стать как Александр Усик?')
-}else if(sport === 'basketball'){
-    alert('Круто! Ты хочешь стать как Кобби Брайн?')
-}else if(sport === 'soccer'){
-    alert('Круто! Ты хочешь стать как Роналду?')
-}else if(sport === null){
-    alert('Too bad you did not want to enter your favorite sport')
+if (sport === 'boks') {
+    message += 'Круто! Ты хочешь стать как Александр Усик?\n';
+} else if (sport === 'basketball') {
+    message += 'Круто! Ты хочешь стать как Кобби Брайант?\n';
+} else if (sport === 'soccer') {
+    message += 'Круто! Ты хочешь стать как Роналду?\n';
+} else if (sport === null) {
+    message += 'Too bad you did not want to enter your favorite sport\n';
+} else {
+    message += `Твой любимый вид спорта -  ${sport} \n`;
 }
+
+alert(message);
