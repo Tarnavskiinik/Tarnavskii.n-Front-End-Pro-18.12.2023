7f87d3608827eaf5bfd46401997a4ca411983afc
Nikolay <nik.tarnavsky@gmail.com>
2024-01-16 16:11:19 +0200

    Homework done

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
-const characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
-const generateKey = (length) => {
+const generateKey = (length, characters) => {
     let randomString = "";
-
-    for (let i = 1; i < length; i++) {
-        randomString += characters[Math.floor(Math.random() * characters.length)];
+    
+    for (let i = 0; i < length; i++) {
+    randomString += characters[Math.floor(Math.random() * characters.length)];
     }
-
+    
     return randomString;
-};
-const key = generateKey(16, characters);
-console.log(key);
+    };
+    
+    const characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
+    const key = generateKey(16, characters);
+    console.log(key);
