c86305ecc71da4cbf4dfd6c11d3a04b4933e6167
Nikolay <nik.tarnavsky@gmail.com>
2024-02-13 19:14:40 +0200

    Homework done

---
+++ index.js
@@ -0,0 +1,47 @@
+class Person {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    info() {
+        console.log(`Ім'я: ${this.name}, Вік: ${this.age}`);
+    }
+}
+
+class Car {
+    constructor(brand, model, year, licensePlate, owner) {
+        this.brand = brand;
+        this.model = model;
+        this.year = year;
+        this.licensePlate = licensePlate;
+        this.owner = null;
+        if(owner.age >= 18){
+            this.owner = owner
+        } else{
+            console.log('Вам немає 18 років');
+        }
+    }
+    ownerInfo(){
+        console.log(`Марка: ${this.brand}, Модель: ${this.model}, Рік виписку: ${this.year}, Номерний знак: ${this.licensePlate}`);
+        if (this.owner) {
+            console.log('Інформація про власника:');
+            console.log(this.owner.info());
+        } else {
+            console.log('Цей автомобіль не має власника');
+        }
+    }
+}
+
+
+let person = new Person('Іван', 30);
+let secondPerson = new Person("Марія", 17);
+
+let car = new Car('Toyota', 'Corolla', 2015, 'ВС1234АА', person);
+let secondCar = new Car('Honda', 'Civic', 2020, 'ВС5678ВВ', secondPerson);
+
+console.log('Інформація про автомобіль 1:');
+car.ownerInfo();
+console.log('                ');
+console.log('Інформація про автомобіль 2:');
+secondCar.ownerInfo();
