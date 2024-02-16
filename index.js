5ec32ff01122861aa6c6f2000f567698e7c696b1
Nikolay <nik.tarnavsky@gmail.com>
2024-02-16 16:43:12 +0200

    Homework done

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
 class SuperMath {
-    input() {
-        const X = parseFloat(prompt("Введите число X:"));
-        const Y = parseFloat(prompt("Введите число Y:"));
-        let znak = prompt("Введите оператор (+, -, /, *, %):");
-        if(znak){
-            console.log(this.calculate(X, Y, znak));
-        } else{
-            alert("Вы не ввели оператор");
-            this.input()
+    check(obj) {
+        if (this.isValidOperator(obj.znak)) {
+            console.log(`Вы хотите выполнить действие ${obj.znak} с числами ${obj.X} и ${obj.Y}?`);
+            const answer = prompt("Введите 'да' или 'нет':");
+            if (answer.toLowerCase() === 'да') {
+                console.log(this.calculate(obj.X, obj.Y, obj.znak));
+            } else {
+                this.input();
+            }
+        } else {
+            alert("Неверный оператор. Пожалуйста, введите один из следующих операторов: +, -, /, *, %.");
+            this.input();
         }
+    }
+
+    input() {
+        const X = parseFloat(prompt("Введите первое число:"));
+        const Y = parseFloat(prompt("Введите второе число:"));
+        const znak = prompt("Введите оператор (+, -, /, *, %):");
+        const obj = { X, Y, znak };
+        this.check(obj);
     }
 
     calculate(X, Y, znak) {
@@ -24,11 +35,17 @@ class SuperMath {
             case "%":
                 return X % Y;
             default:
-                alert("Неверный оператор. Пожалуйста, введите один из следующих операторов: +, -, /, *, %.");
-                return this.input();
+                return "Некорректный оператор";
         }
+    }
+
+    isValidOperator(znak) {
+        const operators = ["+", "-", "/", "*", "%"];
+        return operators.includes(znak);
     }
 }
 
-const p = new SuperMath();
-p.input();
+
+
+const math = new SuperMath();
+math.input();
