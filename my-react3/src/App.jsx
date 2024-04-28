2ea0cfebe721b8d49c83c169afbd7363f8db9819
Nikolay <nik.tarnavsky@gmail.com>
2024-04-28 12:05:40 +0300

    Homework done

---
+++ my-react3/src/App.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+import './App.css'
+import Contact from './components/Contact'
+
+function App() {
+
+  return (
+    <>
+      <Contact/>
+    </>
+  )
+}
+
+export default App
