370a2bce28ca4ec42fd25d3a767ab21af8a41eef
Nikolay <nik.tarnavsky@gmail.com>
2024-05-20 18:26:54 +0300

    done

--- nikolay_test/src/pages/PostPage/PostPage.jsx
+++ nikolay_test/src/pages/PostPage/PostPage.jsx
@@ -1,30 +1,22 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from 'react-router-dom';
 import style from './PostPage.module.css'
+import { useGetData } from "../../hooks/useGetData";
 
 export const PostPage = () => {
     const { postId } = useParams();
-    const [post, setPost] = useState({});
-    const [comments, setComments] = useState([]);
+    const {data:post , loading:postLoading, error: postError} = useGetData(`https://jsonplaceholder.typicode.com/posts/${postId}`)
+    const {data:commentsPost , loading:commentsLoading, error: commentsError} = useGetData(`https://jsonplaceholder.typicode.com/comments?postId=${postId}`)
 
-    useEffect(() => {
-        fetch(`https://jsonplaceholder.typicode.com/posts/${postId}`)
-            .then(response => response.json())
-            .then(data => setPost(data))
-            .catch(error => console.error("Error fetching post:", error));
-        
-        fetch(`https://jsonplaceholder.typicode.com/comments?postId=${postId}`)
-            .then(response => response.json())
-            .then(data => setComments(data))
-            .catch(error => console.error("Error fetching comments:", error));
-    }, [postId]);
+    if (postLoading || commentsLoading) return <div>Loading...</div>;
+    if (postError || commentsError) return <div>Error: {postError || commentsError}</div>;
 
     return (
         <div>
             <h3 className={style.postWrapPostTitle}>{post.title}</h3>
             <p className={style.postWrapText}>{post.body}</p>
             <div>
-                {comments.map(comment => (
+                {commentsPost.map(comment => (
                     <div key={comment.id}>
                         <p>{comment.body}</p>
                     </div>
